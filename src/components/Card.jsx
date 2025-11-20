e721d770620ae0b821bafada56e46c81bd48c131
Amarjit Gupta <akg433787@gmail.com>
2025-11-20 17:08:43 +0530

    first commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import sans from '../assets/sans.png';
 
 export const Card1 = () => {
   return (
-    <div className="relative rounded-[40px] h-197 lg:h-110 w-79 lg:w-200 flex flex-col lg:flex-row mt-15 lg:mt-10 m-auto justify-between items-center bg-[#262c23] transform transition-all duration-500 hover:scale-101 hover:shadow-lg hover:-translate-y-1  hover:shadow-black/40">
+    <div className="border border-gray-600 rounded-[40px] h-197 lg:h-110 w-79 lg:w-200 flex flex-col lg:flex-row mt-15 lg:mt-10 m-auto justify-between items-center bg-[#262c23] transform transition-all duration-500 hover:scale-101 hover:shadow-lg hover:-translate-y-1  hover:shadow-black/40">
       <div className=" h-110 w-79 lg:w-130 -translate-y-17 lg:-translate-x-11">
         <img src={heroimg} alt="o2 card image" className="w-79 lg:w-125 h-100 lg:h-125" />
       </div>
@@ -15,15 +15,13 @@ export const Card1 = () => {
         <p>Many plants can help filter out pollutants and toxins from the air, such as formaldehyde, benzene, and trichloroethylene. This makes the air cleaner and healthier to breathe.</p>
         <div className=" flex justify-between pr-16"><button className="border border-gray-500 px-4 py-1 rounded">Explore</button><p className='mt-2 lg:mt-0'>01/<span className="text-[14px]">04</span></p></div>
       </div>
-      <span className="absolute top-0 left-0 w-60 h-60 border-t-[0.1px] border-l-[0.1px] border-gray-600 rounded-tl-[40px]"></span>
-      <span className="absolute bottom-0 right-0 w-60 h-60 border-b-[0.1px] border-r-[0.1px] border-gray-600 rounded-br-[40px]"></span>
     </div>
   );
 }
 
 export const Card2 = () => {
   return (
-    <div className="relative rounded-[40px] h-197 lg:h-110 w-79 lg:w-200 flex flex-col lg:flex-row mt-15 lg:mt-10 m-auto justify-between items-center bg-[#262c23] transform transition-all duration-500 hover:scale-101 hover:shadow-lg hover:-translate-y-1  hover:shadow-black/40">
+    <div className="border border-gray-600 rounded-[40px] h-197 lg:h-110 w-79 lg:w-200 flex flex-col lg:flex-row mt-15 lg:mt-10 m-auto justify-between items-center bg-[#262c23] transform transition-all duration-500 hover:scale-101 hover:shadow-lg hover:-translate-y-1  hover:shadow-black/40">
       <div className=" h-110 w-79 lg:w-130 -translate-y-17 lg:-translate-x-11">
         <img src={card1} alt="o2 card image" className="w-79 lg:w-125 h-100 lg:h-125" />
       </div>
@@ -33,15 +31,13 @@ export const Card2 = () => {
         <p>Many plants can help filter out pollutants and toxins from the air, such as formaldehyde, benzene, and trichloroethylene. This makes the air cleaner and healthier to breathe.</p>
         <div className="border border-[#262c23] flex justify-between pr-16"><button className="border border-gray-500 px-4 py-1 rounded">Explore</button><p className='mt-2 lg:mt-0'>02/<span className="text-[14px]">04</span></p></div>
       </div>
-      <span className="absolute top-0 left-0 w-60 h-60 border-t-[0.1px] border-l-[0.1px] border-gray-600 rounded-tl-[40px]"></span>
-      <span className="absolute bottom-0 right-0 w-60 h-60 border-b-[0.1px] border-r-[0.1px] border-gray-600 rounded-br-[40px]"></span>
     </div>
   );
 }
 
 export const Card3 = () => {
   return (
-    <div className="relative rounded-[40px] h-197 lg:h-110 w-79 lg:w-200 flex flex-col lg:flex-row mt-15 lg:mt-10 m-auto justify-between items-center bg-[#262c23] transform transition-all duration-500 hover:scale-101 hover:shadow-lg hover:-translate-y-1  hover:shadow-black/40">
+    <div className="border border-gray-600 rounded-[40px] h-197 lg:h-110 w-79 lg:w-200 flex flex-col lg:flex-row mt-15 lg:mt-10 m-auto justify-between items-center bg-[#262c23] transform transition-all duration-500 hover:scale-101 hover:shadow-lg hover:-translate-y-1  hover:shadow-black/40">
       <div className=" h-110 w-79 lg:w-130 -translate-y-17 lg:-translate-x-11">
         <img src={card2} alt="o2 card image" className="w-79 lg:w-125 h-100 lg:h-125" />
       </div>
@@ -51,15 +47,13 @@ export const Card3 = () => {
         <p>Many plants can help filter out pollutants and toxins from the air, such as formaldehyde, benzene, and trichloroethylene. This makes the air cleaner and healthier to breathe.</p>
         <div className="border border-[#262c23] flex justify-between pr-16"><button className="border border-gray-500 px-4 py-1 rounded">Explore</button><p className='mt-2 lg:mt-0'>03/<span className="text-[14px]">04</span></p></div>
       </div>
-      <span className="absolute top-0 left-0 w-60 h-60 border-t-[0.1px] border-l-[0.1px] border-gray-600 rounded-tl-[40px]"></span>
-      <span className="absolute bottom-0 right-0 w-60 h-60 border-b-[0.1px] border-r-[0.1px] border-gray-600 rounded-br-[40px]"></span>
     </div>
   );
 }
 
 export const Card4 = () => {
   return (
-    <div className="relative rounded-[40px] h-197 lg:h-110 w-79 lg:w-200 flex flex-col lg:flex-row mt-15 lg:mt-10 m-auto justify-between items-center bg-[#262c23] transform transition-all duration-500 hover:scale-101 hover:shadow-lg hover:-translate-y-1  hover:shadow-black/40">
+    <div className="border border-gray-600 rounded-[40px] h-197 lg:h-110 w-79 lg:w-200 flex flex-col lg:flex-row mt-15 lg:mt-10 m-auto justify-between items-center bg-[#262c23] transform transition-all duration-500 hover:scale-101 hover:shadow-lg hover:-translate-y-1  hover:shadow-black/40">
       <div className=" h-110 w-79 lg:w-130 -translate-y-17 lg:-translate-x-11">
         <img src={sans} alt="o2 card image" className="w-79 lg:w-125 h-100 lg:h-125" />
       </div>
@@ -69,8 +63,6 @@ export const Card4 = () => {
         <p>Many plants can help filter out pollutants and toxins from the air, such as formaldehyde, benzene, and trichloroethylene. This makes the air cleaner and healthier to breathe.</p>
         <div className="border border-[#262c23] flex justify-between pr-16"><button className="border border-gray-500 px-4 py-1 rounded">Explore</button><p className='mt-2 lg:mt-0'>04/<span className="text-[14px]">04</span></p></div>
       </div>
-      <span className="absolute top-0 left-0 w-60 h-60 border-t-[0.1px] border-l-[0.1px] border-gray-600 rounded-tl-[40px]"></span>
-      <span className="absolute bottom-0 right-0 w-60 h-60 border-b-[0.1px] border-r-[0.1px] border-gray-600 rounded-br-[40px]"></span>
     </div>
   );
 }
